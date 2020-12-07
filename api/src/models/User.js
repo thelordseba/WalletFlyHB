c58934439fecafe0ca138e0c309fbb175efc6be3
sebaTruisi <sebatruisi@gmail.com>
2020-12-07 18:47:16 -0300

    07122020-18:47 Agrego Api

---
+++ api/src/models/User.js
@@ -0,0 +1,56 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  sequelize.define("user", {
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    firstName: {
+      type: DataTypes.STRING,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+    },
+    birthdate: {
+      type: DataTypes.STRING,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        is: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/, //Min 8 caracteres, 1 number, 1 uppercase
+      },
+    }, 
+    documentType: {
+      type: DataTypes.STRING,
+    },
+    documentNumber: {
+      type: DataTypes.INTEGER,
+      unique: true
+    },
+    phone: {
+      type: DataTypes.STRING,
+    },
+    address: {
+      type: DataTypes.STRING,
+    },
+    addressNumber: {
+      type: DataTypes.INTEGER,
+    },
+    postalCode:{
+      type: DataTypes.INTEGER,
+    },
+    city:{
+      type: DataTypes.STRING,
+    },
+    province:{
+      type: DataTypes.STRING,
+    },
+    country:{
+      type: DataTypes.STRING,
+    },
+  });
+};
+
