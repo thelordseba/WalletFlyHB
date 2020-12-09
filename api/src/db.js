0148681e8827fb20118e91f5ef2bad8a486865ad
sebaTruisi <sebatruisi@gmail.com>
2020-12-09 12:52:16 -0300

    09122020-12:52 Creacion de rutas get user post account

--- api/src/db.js
+++ api/src/db.js
@@ -7,7 +7,9 @@ const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
-  dialect: 'mysql'
+  dialect: 'mysql',
+  logging: false, // set to console.log to see the raw SQL queries
+  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
 
 const basename = path.basename(__filename);
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,10 +2,12 @@ const { Router } = require("express");
 const router = Router();
 // importar todas los enrutadores;
 const userRouter = require("./user.js");
+const accountRouter = require("./account.js");
 
 // cargar cada enrutador en una ruta
 // i.e: router.use('/auth', authRouter);
 
 router.use("/users", userRouter);    
+router.use('/accounts', accountRouter);
 
 module.exports = router;
