a49c3adfbd411de8528334d7070f3dd6fd41ff6f
Maico <maicoloncomilla@gmail.com>
2020-12-11 19:37:55 -0300

    (Client/src/footer)

--- client/App.js
+++ client/App.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { StyleSheet, Text, View, Platform } from "react-native";
-import { Router, Route, Link } from "./react-router";
+import { Router } from "./react-router";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import Login from "./src/screens/Login";
@@ -9,40 +8,44 @@ import UpdateUser2 from "./src/screens/UpdateUser2";
 import CreateUser from "./src/screens/CreateUser";
 import UserProfile from "./src/screens/userProfile/UserProfile";
 import Footer from './src/screens/footer/Footer';
-import DatosPersonales from "./src/screens/userProfile/DatosPersonales";
-import Transacciones from './src/screens/main/Transacciones';
-import Home from './src/screens/main/Home';
-
 
 const Stack = createStackNavigator();
-
 function MyStack() {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Home" component={Home}/>
-      <Stack.Screen name="Transacciones" component={Transacciones}/>
+      {/* <Stack.Screen name="Home" component={Home}/>
+      <Stack.Screen name="Transacciones" component={Transacciones}/> */}
       <Stack.Screen name="Login" component={Login} />
       <Stack.Screen name="CreateUser" component={CreateUser} />
       <Stack.Screen name="UpdateUser" component={UpdateUser} />
       <Stack.Screen name="UpdateUser2" component={UpdateUser2} />
-      <Stack.Screen name="DatosPersonales" component={DatosPersonales}/>
+      
       <Stack.Screen name="UserProfile" component={UserProfile} />
-      <Stack.Screen name="footer" component={Footer}/>
     </Stack.Navigator>
   );
 }
-
-// const Home = () => <Text>Home</Text>;
-
-const About = () => <Text>About</Text>;
+// Si el usuaario no tiene un Token mostrara la pantalla 
+const user = true;
 
 const App = () => (
-  <Router>
-    <NavigationContainer>
-      <MyStack />
-      <Footer />
-    </NavigationContainer>
-  </Router>
+  <>
+    {
+      user ? (
+        // Usuario Logeado
+        <Router>
+          <NavigationContainer>
+            <Footer />
+          </NavigationContainer>
+        </Router>
+      ) : (
+        // Usuario deslogeado
+          <Router>
+            <NavigationContainer>
+              <MyStack />
+            </NavigationContainer>
+          </Router>
+        )
+    }
+  </>
 );
-
 export default App;
