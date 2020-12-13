31dc8a463010aa9de606667cecf857233b8485e5
sebaTruisi <sebatruisi@gmail.com>
2020-12-13 16:54:48 -0300

    13/12/2020-16:54 oiniendo en flase el force de la bd

--- api/index.js
+++ api/index.js
@@ -31,7 +31,7 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js'); 
 
 // Sincronizadno todos los modelos a la vez.
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   server.listen(3001, () => {
     console.log('%s listening at 3001'); 
   });
