005a9a5a2be1f153fffd6aed0cade02ab0d59626
Maico <maicoloncomilla@gmail.com>
2020-12-15 13:07:14 -0300

    Redux conectado al UserProfile y al main

--- client/src/screens/main/screens/userProfile/DatosPersonales.js
+++ client/src/screens/main/screens/userProfile/DatosPersonales.js
@@ -1,28 +1,48 @@
 import React from 'react';
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { useSelector } from 'react-redux';
 
 export default function DatosPersonales(props){
+    const email = useSelector(state => state.email)
+    const user = useSelector(state => state.user)
+    const userDirection = useSelector(state => state.userDirection)
     return (
         <SafeAreaView>
             <View style={s.container}>
                <Text>Email</Text>
-               <Text>maicoloncomilla@gmail.com</Text>
+               <Text>{email}</Text>
+            </View>
+            <View style={s.container}>
+               <Text>Fecha de nacimiento</Text>
+               <Text>{user.birthdate}</Text>
+            </View>
+            <View style={s.container}>
+               <Text>Telefono</Text>
+               <Text>{user.phone}</Text>
+            </View>
+            <View style={s.container}>
+               <Text>Nº de documento</Text>
+               <Text>{user.documentNumber}</Text>
             </View>
             <View style={s.container}>
                <Text>Domicilio</Text>
-               <Text>Calle falsa 123</Text>
+               <Text>{userDirection.address} Nº {userDirection.addressNumber}</Text>
             </View>
             <View style={s.container}>
                <Text>Código Postal</Text>
-               <Text>8360</Text>
+               <Text>{userDirection.postalCode}</Text>
             </View>
             <View style={s.container}>
                <Text>Localidad</Text>
-               <Text>Choele Choel</Text>
+               <Text>{userDirection.city}</Text>
             </View>
             <View style={s.container}>
                <Text>Provincia</Text>
-               <Text>Rio Negro</Text>
+               <Text>{userDirection.province}</Text>
+            </View>
+            <View style={s.container}>
+               <Text>País</Text>
+               <Text>{userDirection.country}</Text>
             </View>
         </SafeAreaView>
     )
