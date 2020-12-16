f53fe8059667e7e922381e428c60b69d51520a9d
Maico <maicoloncomilla@gmail.com>
2020-12-16 14:14:20 -0300

    Menu Desplegable, hay que hacer funcionar Expo en el celular para probar si funca

--- client/App.js
+++ client/App.js
@@ -2,18 +2,20 @@ import React from "react";
 import { Router } from "./react-router";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import Login from "./src/screens/Login";
 import UpdateUser from "./src/screens/UpdateUser";
 import UpdateUser2 from "./src/screens/UpdateUser2";
 import Register from "./src/screens/Register";
-import UserProfile from "./src/screens/main/screens/userProfile/UserProfile";
 import Main from "./src/screens/main/Main";
 import AuthEmail from "./src/screens/AuthEmail";
 import MainView from "./src/screens/MainView";
 import { store } from './src/reducer/Reducer';
 import { Provider, useSelector } from 'react-redux';
+import DrawerContent from "./src/screens/main/screens/DrawerContent";
 
 const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
 function MyStack() {
   return (
     <Stack.Navigator>
@@ -23,25 +25,36 @@ function MyStack() {
       <Stack.Screen name="AuthEmail" component={AuthEmail} />
       <Stack.Screen name="UpdateUser" component={UpdateUser} />
       <Stack.Screen name="UpdateUser2" component={UpdateUser2} />
-      <Stack.Screen name="UserProfile" component={UserProfile} />
     </Stack.Navigator>
   );
 }
 const AppWrapper = () => {
   return (
     <Provider store={store}>
-      <App/>
+      <App />
     </Provider>
   )
 }
+
 const App = () => {
   const user = useSelector(state => state.userDirection)
   const userLogin = useSelector(state => state.userLogin)
-  
+  // const user = true;
   return (
-      <Router>
-        <NavigationContainer>{user || userLogin ? <Main /> : <MyStack />}</NavigationContainer>
-      </Router>
+    <Router>
+      <NavigationContainer>{user || userLogin ? <Main /> : <MyStack />}</NavigationContainer>
+    </Router>
+      //   <Router>
+      //   <NavigationContainer>
+      //     {user || userLogin ?
+      //       <Drawer.Navigator drawerContent={props => <DrawerContent {...props} />} openByDefault>
+      //         <Drawer.Screen name="HomeDrawer" component={Main} />
+      //       </Drawer.Navigator>
+      //       : <MyStack />}
+      //     {/* <Main />  */}
+      //   </NavigationContainer>
+      // </Router>
+
   )
 }
 export default AppWrapper
