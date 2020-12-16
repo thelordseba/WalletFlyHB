fdd455614f0f2158ab1b7bdae12c9c6e3867b096
SantiagoRiveros <santiagoriverosjasis@gmail.com>
2020-12-16 15:56:54 -0300

    Rutas de contacts

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -3,15 +3,15 @@ const router = Router();
 // importar todas los enrutadores;
 const userRouter = require("./user.js");
 const accountRouter = require("./account.js");
+const contactsRouter = require("./contacts.js");
 
 //const productRouter = require("./product"); //ESTO ES UN EJEMPLO - BORRAR
 
 // cargar cada enrutador en una ruta
 // i.e: router.use('/auth', authRouter);
 
-
-router.use("/users", userRouter);    
-router.use('/accounts', accountRouter);
-
+router.use("/users", userRouter);
+router.use("/accounts", accountRouter);
+router.use("/contacts", contactsRouter);
 
 module.exports = router;
