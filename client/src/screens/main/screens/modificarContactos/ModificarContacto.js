cdf9f458d9341a6d3c91844e4092e1c4fb8ce8ef
sebaTruisi <sebatruisi@gmail.com>
2021-01-06 23:48:12 -0300

    Imagen de avatar de Detalles de contacto

--- client/src/screens/main/screens/modificarContactos/ModificarContacto.js
+++ client/src/screens/main/screens/modificarContactos/ModificarContacto.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, TextInput, View } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import { Appbar, Avatar } from "react-native-paper";
@@ -6,6 +6,8 @@ import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityI
 import axios from "axios";
 import api from "../../../../reducer/ActionCreator";
 import { useDispatch, useSelector } from "react-redux";
+import firebase from "firebase/app";
+import "firebase/storage";
 
 export default function ModificarContacto({ navigation, route }) {
   const { CONTACTOS } = api;
@@ -14,11 +16,21 @@ export default function ModificarContacto({ navigation, route }) {
   const userId = route.params.idUser;
   const Name = route.params.firstName + " " + route.params.lastName;
   const alias = route.params.alias;
-  const user = useSelector((state) => state.user);
-  const userImage = useSelector((state) => state.userImage);
+  const user = useSelector((state) => state.user); 
   const email = route.params.email;
   const [value, setValue] = useState("");
   const [active, setActive] = useState(false);
+  const [contactImage, setContactImage] = useState(null);
+
+  useEffect(()=>{
+    firebase.storage().ref(`/profileImage/${email}`).getDownloadURL()
+      .then((image) => {  
+        setContactImage(image)
+      })
+      .catch((error) => {
+        setContactImage(null)     
+      });
+  }, [email]);
 
   const handleEdit = (value) => {
     axios
@@ -73,11 +85,9 @@ export default function ModificarContacto({ navigation, route }) {
           <Avatar.Image
             size={100}
             source={{
-              uri: userImage
-                ? userImage
-                : require("../../../../images/Avatar.png"),
-            }}
-          />
+              uri: contactImage ? contactImage : require("../../../../images/Avatar.png"),
+            }}            
+          />         
           <View
             style={{
               display: "flex",
@@ -130,7 +140,7 @@ export default function ModificarContacto({ navigation, route }) {
               }}
             >
               Nombre:
-            </Text>{" "}
+            </Text>
             <Text style={{ color: "#cb3065", fontFamily: "Bree-Serif" }}>
               {Name}
             </Text>
@@ -142,7 +152,7 @@ export default function ModificarContacto({ navigation, route }) {
               }}
             >
               Email:
-            </Text>{" "}
+            </Text>
             <Text style={{ color: "#cb3065", fontFamily: "Bree-Serif" }}>
               {email}
             </Text>
