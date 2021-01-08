5cd378eb699ab21f57b932e4232ede911db566e4
Maico <maicoloncomilla@gmail.com>
2021-01-08 12:42:29 -0300

    Nueva Screen

--- client/src/screens/registro/Registro.js
+++ client/src/screens/registro/Registro.js
@@ -2,14 +2,25 @@ import React from "react";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 import Register from "./screens/Register";
 import Login from "./screens/Login";
+import { Image } from 'react-native';
 
 const Tab = createMaterialTopTabNavigator();
 export default function Registro(props) {
   {
     /* Colocamos Logo o algo para decorar*/
   }
+  const iconPng = require("../../images/Icon.png")
   return (
     <>
+      <Image
+        style={{
+          width: "80%",
+          height: 200,
+          marginLeft: "auto",
+          marginRight: "auto",
+        }}
+        source={iconPng}
+      />
       <Tab.Navigator
         tabBarOptions={{
           labelStyle: { fontFamily: "Bree-Serif", fontSize: 14 },
